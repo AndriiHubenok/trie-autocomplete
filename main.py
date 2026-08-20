5dadd701940775f6d06fbd252560529ae5515898
AndriiHubenok <andrewhu08@gmail.com>
2026-08-20 12:39:33 +0300

    add autocomplete

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import sys
 from radix_trie import RadixTrie
 from trie import Trie
 
-root = RadixTrie()
+root = Trie()
 size_count = 0
 
 for raw in sys.stdin:
     line = raw.rstrip("\n")
     if not line: continue
-    parts = line.split(" ", 1)
-    cmd = parts[0]; arg = parts[1] if len(parts) > 1 else ""
+    parts = line.split(" ")
+    cmd = parts[0]; arg = parts[1] if len(parts) > 1 else ""; arg2 = parts[2] if len(parts) > 2 else ""
     if cmd == "INSERT":
         root.insert(arg)
-        print("OK")
+
+    elif cmd == "INSERT_N":
+        root.insert(arg, int(arg2))
 
     elif cmd == "CONTAINS":
         if root.contains(arg):
@@ -23,6 +25,13 @@ for raw in sys.stdin:
 
     elif cmd == "DELETE":
         root.delete(arg)
+
+    elif cmd == "SUGGEST":
+        results = root.suggest(arg, int(arg2))
+        if len(results) == 0:
+            print("none")
+        else:
+            print(','.join([str(result) for result in results]))
 
     elif cmd == "SIZE":
         print(root.size())
--- trie.py
+++ trie.py
@@ -1,16 +1,20 @@
+from pygments.lexer import words
+
+
 class Trie:
     def __init__(self):
         self.children = {}
         self.frequency = 1
         self.is_end = False
 
-    def insert(self, word: str) -> None:
+    def insert(self, word: str, frequency=1) -> None:
         node = self
         for char in word:
             if char not in node.children:
                 node.children[char] = Trie()
-            else:
-                node.children[char].frequency += 1
+                node.children[char].frequency = frequency
+            # else:
+            #     node.children[char].frequency += frequency
             node = node.children[char]
 
         node.is_end = True
@@ -48,6 +52,18 @@ class Trie:
             index -= 1
 
         return True
+
+    def suggest(self, prefix: str, k: int) -> list:
+        node = self
+        for char in prefix:
+            if char not in node.children:
+                return []
+            node = node.children[char]
+
+        results = []
+        self.dfs(node, prefix, results)
+        results.sort(key=lambda x: (-x.freq, x.word))
+        return results[:k]
 
     def size(self) -> int:
         count = 1 if self.is_end else 0
@@ -90,7 +106,15 @@ class Trie:
 
     def dfs(self, node, prefix, words):
         if node.is_end:
-            words.append(prefix)
+            words.append(WordWithFreq(prefix, node.frequency))
 
         for char, child in node.children.items():
             self.dfs(child, prefix + char, words)
+
+class WordWithFreq:
+    def __init__(self, word, freq):
+        self.word = word
+        self.freq = freq
+
+    def __str__(self):
+        return f'{self.word}({self.freq})'
